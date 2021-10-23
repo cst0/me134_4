896a366da92d704b2d16b60f6c8362c6437a59f1
Chris Thierauf <christopher.thierauf@tufts.edu>
2021-10-23 17:21:29 -0400

    add sim mode to imu node

--- src/imu_node.py
+++ src/imu_node.py
@@ -2,27 +2,24 @@
 
 import rospy
 
-import os
-import sys
-import time
-import smbus
-import warnings
-from math import sqrt, cos, sin
-
-from imusensor.MPU9250 import MPU9250
+from math import cos, sin
 from sensor_msgs.msg import Imu
-import RPi.GPIO as GPIO
+from numpy.random import normal as random #type:ignore
+
+SIMULATION_MODE = True
+try:
+    import smbus #type:ignore
+    from imusensor.MPU9250 import MPU9250 #type:ignore
+    SIMULATION_MODE = False
+except ImportError:
+    print("Could not import hardware dependencies. Assuming we're in sim mode and going from here.")
 
 address = 0x68
 
-bus = smbus.SMBus(1)
-imu = MPU9250.MPU9250(bus, address)
-imu.begin()
-
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(12, GPIO.OUT)
-pwm = GPIO.PWM(12, 50)
-pwm.start(0)
+if not SIMULATION_MODE:
+    bus = smbus.SMBus(1) #type:ignore
+    imu = MPU9250.MPU9250(bus, address) #type:ignore
+    imu.begin()
 
 rospy.init_node('imu_node')
 imu_pub = rospy.Publisher('imu_pub', Imu, queue_size=3)
@@ -35,7 +32,7 @@ def to_quaternion(yaw, pitch, roll):
     cr = cos(roll * 0.5)
     sr = sin(roll * 0.5)
 
-    q = [0, 0, 0, 0];
+    q = [0.0, 0.0, 0.0, 0.0]
     q[0] = sr * cp * cy - cr * sp * sy;
     q[1] = cr * sp * cy + sr * cp * sy;
     q[2] = cr * cp * sy - sr * sp * cy;
@@ -52,24 +49,36 @@ def main():
     msg.header.frame_id = 'imu'
     while not rospy.is_shutdown():
         try:
-            imu.readSensor()
-            imu.computeOrientation()
-        except Exception as e:
+            if not SIMULATION_MODE:
+                imu.readSensor()
+                imu.computeOrientation()
+        except Exception:
             pass
 
         msg.header.seq += 1
         msg.header.stamp = rospy.Time.now()
 
-        msg.linear_acceleration.x = imu.AccelVals[0]
-        msg.linear_acceleration.y = imu.AccelVals[1]
-        msg.linear_acceleration.z = imu.AccelVals[2]
+        if not SIMULATION_MODE:
+            msg.linear_acceleration.x = imu.AccelVals[0]
+            msg.linear_acceleration.y = imu.AccelVals[1]
+            msg.linear_acceleration.z = imu.AccelVals[2]
 
-        q = to_quaternion(imu.yaw, imu.pitch, imu.roll)
+            q = to_quaternion(imu.yaw, imu.pitch, imu.roll)
 
-        msg.orientation.x = q[0]
-        msg.orientation.y = q[1]
-        msg.orientation.z = q[2]
-        msg.orientation.w = q[3]
+            msg.orientation.x = q[0]
+            msg.orientation.y = q[1]
+            msg.orientation.z = q[2]
+            msg.orientation.w = q[3]
+
+        else:
+            msg.linear_acceleration.x = random()
+            msg.linear_acceleration.y = random()
+            msg.linear_acceleration.z = random()
+
+            msg.orientation.x = random()
+            msg.orientation.y = random()
+            msg.orientation.z = random()
+            msg.orientation.w = random()
 
         imu_pub.publish(msg)
 
