72af4f408912bc5ab5e5c496b20da23239ed9e1a
Chris Thierauf <christopher.thierauf@tufts.edu>
2021-11-01 12:00:22 -0400

    publish rpy values after conversion

--- src/imu_to_tilt.py
+++ src/imu_to_tilt.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 NO_TF = True # hack for now: hard-code tf
-TILT_AXIS = 1
+
+ROLL = 0
+PITCH = 1
+YAW = 2
 
 import rospy
 if not NO_TF:
     import tf2_ros
 from tf.transformations import euler_from_quaternion, quaternion_from_euler, quaternion_multiply, quaternion_about_axis
 from sensor_msgs.msg import Imu
+from std_msgs.msg import Float32
 from geometry_msgs.msg import PoseStamped, Quaternion
 from me134.msg import SegwayTilt
 
@@ -15,6 +19,9 @@ class ImuToTilt(object):
     def __init__(self):
         self.imu_sub = rospy.Subscriber("imu_raw", Imu, self.imu_cb, queue_size=5)
         self.tilt_pub = rospy.Publisher("imu_tilt", SegwayTilt, queue_size=5)
+        self.tilt_roll_pub = rospy.Publisher("imu/tilt_roll", Float32, queue_size=5)
+        self.tilt_pitch_pub = rospy.Publisher("imu/tilt_pitch", Float32, queue_size=5)
+        self.tilt_yaw_pub = rospy.Publisher("imu/tilt_yaw", Float32, queue_size=5)
 
         if not NO_TF:
             self.tf_buffer = tf2_ros.Buffer()
@@ -23,12 +30,18 @@ class ImuToTilt(object):
     def imu_cb(self, msg):
         tilt = SegwayTilt()
         tilt.source = 'imu_gyro'
-        tilt.radians = euler_from_quaternion([
+        rpy = euler_from_quaternion([
             msg.orientation.x,
             msg.orientation.y,
             msg.orientation.z,
             msg.orientation.w,
-            ])[TILT_AXIS]
+            ])
+
+        self.tilt_roll_pub.publish(Float32(rpy[ROLL]))
+        self.tilt_pitch_pub.publish(Float32(rpy[PITCH]))
+        self.tilt_yaw_pub.publish(Float32(rpy[YAW]))
+
+        tilt.radians = rpy[PITCH]
         tilt.radians_vel = msg.angular_velocity.y
 
         self.tilt_pub.publish(tilt)
