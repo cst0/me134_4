f877f08e7f2a4344143717f5b877be89c01f2ba7
Chris Thierauf <christopher.thierauf@tufts.edu>
2021-11-02 10:21:30 -0400

    launchfile and controller tweaks

--- src/joystick_manual_animatronics.py
+++ src/joystick_manual_animatronics.py
@@ -11,20 +11,22 @@ class JoystickConverter(object):
     def controller_cb(self, controller_msg:Controller):
         # map controller joysticks to arm movements
         torso_msg = TorsoState()
+        torso_msg.lock_wheels = False
         if any(b for b in controller_msg.button_state[4:8]):
             # one of the triggers is pressed. put us in animatronic mode
             torso_msg.chest = controller_msg.axis_state[0]
             torso_msg.left_elbow = controller_msg.axis_state[1]
             torso_msg.head = controller_msg.axis_state[2]
             torso_msg.right_elbow = controller_msg.axis_state[3]
-            torso_msg.tail = torso_msg.DOWN
-
-        torso_msg.tail = torso_msg.UP
+            torso_msg.lock_wheels = True
 
         self.torso_pub.publish(torso_msg)
 
 def main():
+    rospy.init_node('controller_converter')
     JoystickConverter()
     rospy.spin()
 
 
+if __name__ == '__main__':
+    main()
